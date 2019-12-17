42635e18a9647c70e92dfe08c588561b2dfd4c26
Jaired Jawed <jaireddjawed.7@gmail.com>
2019-12-17 12:56:53 -0800

    Update Deck.h

--- Deck.h
+++ Deck.h
@@ -17,66 +17,69 @@
 #include "Card.h"
 using namespace std;
 
-struct Cards {
-    string name; // "Aces"
-    int value; // 11
 
-    // tree children
-    Card *spade;
-    Card *heart;
-    Card *diamond;
-    Card *club;
+// quarternary tree
+struct Node {
+    Card *data;
+    
+    Node *child1;
+    Node *child2;
+    Node *child3;
+    Node *child4;
 };
 
-struct Cards* newNode (int value) {
-    // Allocate memory for new CardType
-    struct Cards* cards = (struct Cards*)malloc(sizeof(struct Cards)); 
+// add a new card to the tree
+struct Node* newNode(Card* card) {
+    struct Node* temp = new Node;
     
-    // assign the name and value to the card type
-    cards->value = value;
-    
-    // assign the card children as null
-    cards->club = NULL;
-    cards->diamond = NULL;
-    cards->heart = NULL;
-    cards->spade = NULL;
-    
-    return (cards);
+    if (card == NULL) {
+        return temp;
+    }
+
+    temp->data = new Card;
+    temp->data->name = card->name;
+    temp->data->value = card->value;
+
+    return temp;
 }
 
-void insert(struct Cards* temp, Card* card, string cardType) {
-    queue<struct Cards*> q;
+// inserts a new card into our tree
+void insert(struct Node* temp, Card* card) {
+    queue<struct Node*> q;
     q.push(temp);
     
     while (!q.empty()) { 
-        struct Cards* temp = q.front(); 
+        struct Node* temp = q.front(); 
         q.pop(); 
   
-        // Add a new child depending on the card type
-        if (cardType == "diamond") {
-            temp->diamond = card;
+        if (!temp->child1) {
+            temp->child1 = newNode(card);
             break;
+        } else {
+            q.push(temp->child1);
         }
         
-        if (cardType == "spade") {
-            temp->spade = card;
+        if (!temp->child2) {
+            temp->child2 = newNode(card);
             break;
-        }
-
-        if (cardType == "club") {
-            temp->club = card;
-            break;
+        } else {
+            q.push(temp->child2);
         }
         
-        if (cardType == "heart") {
-            temp->heart = card;
+        if (!temp->child3) {
+            temp->child3 = newNode(card);
             break;
+        } else {
+            q.push(temp->child3);
         }
-    } 
-}
-
-void createCardTypes() {
-    
+        
+        if (!temp->child4) {
+            temp->child4 = newNode(card);
+            break;
+        } else {
+            q.push(temp->child4);
+        }
+    }
 }
 
 class Deck {
@@ -87,33 +90,12 @@ private:
     map<string, int> cardTypes;
     static int getRandomNum(int);
 public:
-    Deck();
     stack<Card *> cards;
     void setupCardTypes();
     void setupDeck(int);
     void shuffleDeck();
+    void addTreeToStack(struct Node *);
 };
-
-Deck::Deck() {
-    /*
-    // create each card name and type
-    struct Cards *Aces = newNode("Ace", 11);
-    Aces->name = "Aces";
-
-
-    struct Cards *Twos = newNode("Two", 11);
-    Twos->name = "Two";
-    
-        struct Cards *Aces = newNode("Ace", 11);
-    Aces->name = "Aces";
-    
-    
-    Card* card = new Card;
-    card->name = "Ace of spades";
-    card->value = 11;
-
-    insert(Aces, card, "Ace");*/
-}
 
 // generates a random number for shuffling the cards
 int Deck::getRandomNum(int i) { 
@@ -122,6 +104,9 @@ int Deck::getRandomNum(int i) {
 
 // push all cardTypes to the map
 void Deck::setupCardTypes() {
+    // a map of all the card types, ace is not included because
+    // those will be the top nodes for our tree
+    
     cardTypes.insert(pair<string, int>("2", 2));
     cardTypes.insert(pair<string, int>("3", 3));
     cardTypes.insert(pair<string, int>("4", 4));
@@ -134,23 +119,71 @@ void Deck::setupCardTypes() {
     cardTypes.insert(pair<string, int>("Jack", 10));
     cardTypes.insert(pair<string, int>("Queen", 10));
     cardTypes.insert(pair<string, int>("King", 10));
-    cardTypes.insert(pair<string, int>("Ace", 11));
 }
 
 // creates a deck from the map cardTypes based on the number of decks that the user enters
 void Deck::setupDeck(int numDecks) {
     // there are four cards with the same number per deck
-    int numCards = numDecks * 4;
     map<string, int>::iterator cardType;
     
+    Card* ace = new Card;
+    ace->name = "Ace";
+    ace->value = 11;
+
+    struct Node* root = newNode(NULL);
+
+    struct Node* firstAce = newNode(ace);
+    struct Node* secondAce = newNode(ace);
+    struct Node* thirdAce = newNode(ace);
+    struct Node* fourthAce = newNode(ace);
+
+    root->child1 = firstAce;
+    root->child2 = secondAce;
+    root->child3 = thirdAce;
+    root->child4 = fourthAce;
+    
     for (cardType = cardTypes.begin(); cardType != cardTypes.end(); ++cardType) {
-        for (int i = 1; i <= numCards; i++) {
-            Card* card = new Card;
-            card->name = cardType->first;
-            card->value = cardType->second;
-            cards.push(card);
-        }
+        Card *card1 = new Card;
+        card1->name = cardType->first;
+        card1->value = cardType->second;
+        insert(firstAce, card1);
+        
+        Card *card2 = new Card;
+        card2->name = cardType->first;
+        card2->value = cardType->second;
+        insert(secondAce, card2);
+        
+        Card *card3 = new Card;
+        card3->name = cardType->first;
+        card3->value = cardType->second;
+        insert(thirdAce, card3);
+        
+        Card *card4 = new Card;
+        card4->name = cardType->first;
+        card4->value = cardType->second;
+        insert(fourthAce, card4);
     }
+
+    for (int i = 1; i <= numDecks; i++) {
+        // traverse the tree and add each card to the stack
+        addTreeToStack(root->child1);
+        addTreeToStack(root->child2);
+        addTreeToStack(root->child3);
+        addTreeToStack(root->child4);
+    }
+}
+
+void Deck::addTreeToStack(struct Node* node) {
+    if (node == NULL) {
+        return;
+    }
+
+    Card* card = new Card;
+    card->name = node->data->name;
+    card->value = node->data->value;
+    cards.push(card);
+
+    addTreeToStack(node->child1);
 }
 
 // randomly shuffles the created deck
